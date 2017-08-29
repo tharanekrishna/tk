2acd293cded95ef39e6c06caa38d4194fef35398
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2017-08-29 14:45:14 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
-    char c;
-    printf("Enter a character: ");
-    scanf("%c",&c);
-
-    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-        printf("%c is an alphabet.",c);
-    else
-        printf("%c is not an alphabet.",c);
-
-    return 0;
+char c;
+    clrscr();
+printf("Enter the character: ");
+scanf("%c",&c);
+if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+printf("%c is an alphabet.",c);
+else
+printf("%c is not an alphabet.",c);
+getych();
 }
