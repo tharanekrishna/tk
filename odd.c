8e330c94ce7a94b497266f2f2dd4d39a5a06fadc
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-01-17 16:51:38 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{  
+  int n; 
+  printf("\nodd numbers between 2 intervals");  
+ for(n = 1; n <= 6; n++) 
+{  
+if(n%2 == 1) 
+{ 
+printf("%d ",n); 
+printf("\n odd number between two interval is");
+ }  
+ }  
+return 0;  
+} 
