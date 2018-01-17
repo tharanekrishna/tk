efdc22fb8fec8981322dc4361a5812b0f9ec6811
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-01-17 15:47:42 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -3,6 +3,7 @@
 void main()
 {
  int year;
+ clrscr();
  printf("Enter a year \n");
  scanf("%d", &year);
  if ((year % 400) == 0)
@@ -14,5 +15,6 @@ else if ((year % 4) == 0)
  printf("%d is a leap year \n", year);
 else
 printf("%d is not a leap year \n", year);
-}
+ }
+ getch();
 }
