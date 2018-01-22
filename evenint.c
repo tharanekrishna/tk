f8612e69408643c79341544684cd6a55c4de6d64
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-01-22 15:45:37 +0530

    Create evenint.c

---
+++ evenint.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{  
+    int counter; 
+    printf("Even numbers between 1 to 100\n");  
+   for(counter = 1; counter <= 100; counter++) {  
+      
+        if(counter%2 == 0) { 
+            
+           printf("%d ", counter);  
+        }  
+    }  
+   
+    return 0;  
+} 
