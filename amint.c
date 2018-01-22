c69497075978f167bcb1129a691fc752701d32c8
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-01-22 16:09:44 +0530

    Create amint.c

---
+++ amint.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+int main()
+{
+int num,tem,rem,sum=0;
+clrscr();
+printf("\n enter the number");
+scanf("%d",&num);
+tem=sum;
+while(num!=0)
+for(i=0;i<=num;i++)
+sum=sum+(rem*rem*rem);
+sum=(num/10,num/10);
+printf("% it is an amstrong intervel",tem);
+getch();
+}
