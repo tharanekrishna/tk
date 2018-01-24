69887515543caf73f5cc87b40c028ae31cd75a1b
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-01-24 16:51:14 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("\n enteeeer the string");
+scanf("%d",&a):
+if(a>='0')&&(a<='9'))
+{
+printf("%c yes",a);
+else
+printf("%c no",a);
+}
+return 0;
+}
