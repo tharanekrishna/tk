1884122dd87f3c28d8ed7b7df37d195fd88e7232
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-02 15:48:21 +0530

    Create length.c

---
+++ length.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include,conio.h>
+void main()
+{
+char str[200];
+int a;
+clrscr();
+printf("\n enter the string");
+scanf("%d",&str);
+a=strlen(str);
+printf("\n the length of string is %d",a);
+gech();
+}
