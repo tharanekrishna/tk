8bdd150b63da5221bf679e2d84ec4c0dc458c9de
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-02 16:23:48 +0530

    Create paracount.c

---
+++ paracount.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int b,n=0,i,c,nm=0;
+    char a[100]="hello.world";
+    char j;
+    clrscr();
+    b=strlen(a);
+    for(i=0;i<=10;i++)
+    {
+        if(a[i]=='.')
+        {
+            nm++;
+}  
+}
+printf(" ttl prgf=%d",nm+1);
+getch();
+}
