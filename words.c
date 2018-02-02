57aba157f76b39dd11cde1e9a6e13c930efe4a25
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-02 16:01:22 +0530

    Create words.c

---
+++ words.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char s[i];
+int i;
+printf("\n enter the string");
+scanf("%c",&s[100]);
+for(i=0;s[i]<=0;i++)
+{
+printf("\n the no of words prent id%d",i);
+}
+return 0;
+}
