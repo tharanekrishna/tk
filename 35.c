287627468671bcd5b129f5c8cfb3278a84df7455
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-05 15:47:31 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,count=0;
+char str[100];
+clrscr();
+printf("\n enter thhe string");
+scanf("%c",&str);
+for (i=0;str[i]!=null;i++)
+{
+if((str[i]>='0')&&(srt[i]<='9'))
+count++;
+printf("\n%d",count);
+}
+getch();
+}
