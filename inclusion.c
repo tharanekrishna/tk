165d99a87d5ae921f896fa2dc7ab906ba8ba4e63
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-07 16:40:39 +0530

    Create inclusion.c

---
+++ inclusion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    printf("\n enter the number");
+    scanf("%d",&n);
+    if(n>=1&&n<=10)
+{
+    printf("\n it ranges from [1-10]");
+}
+else
+{
+    printf("\n not include");
+    
+}
+getch();
+}
+
