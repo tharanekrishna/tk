3f3438f2eca7607de41505b1d6039fcfd1f4e31a
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-09 16:18:40 +0530

    Update strcat.c

--- strcat.c
+++ strcat.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
 char a[50],b[40];
+  clrscr();
 printf("enter the first string\n");
 scanf("%s\n",a);
 printf("enter the sec string\n");
 scanf("%s\n",b);
 printf("%s%s\n",a,b);
-return 0;
+getch();
 }
