a9d37d8ac6c4b7a7a420c87a5051d5e7efaf2634
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-09 15:52:17 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("\n enter the num 1:");
+scanf("%d",&a);
+printf("\n enter the nm2:");
+scanf("%d",&b);
+c=a+b;
+printf("\n the additon of two num are:%d",c);
+return 0;
+}
