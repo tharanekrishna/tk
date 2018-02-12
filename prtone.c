e0fcffa362b2be893f7c84808ca41e2f9c844a23
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-12 16:54:50 +0530

    Create prtone.c

---
+++ prtone.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\n enter the num");
+scanf("%d",&a);
+switch(a)
+{
+case 1:
+printf("\none");
+break;
+case 2:
+printf("tw0");
+break;
+}return 0;
+}
