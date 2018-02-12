677088b8c2c52a917a000522fd3d43838a52943e
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-12 16:22:33 +0530

    Create larnsml.c

---
+++ larnsml.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int a[50],n,l=0,s=0;
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[50]);
+}
+l=s=a[50];
+for(i=0;i<n;i++)
+{
+if(a[50]>l)
+{
+l=a[50];
+}
+if(a[50]<s)
+{
+s=a[50];
+}
+}
+printf("\n %d",l/n);
+printf("\n%d",s/n);
+return 0;
+}
