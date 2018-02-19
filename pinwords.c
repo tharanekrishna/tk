3ab7a19683dc26823f5a682cdc809bf5b3c47e7b
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-19 15:45:56 +0530

    Create pinwords.c

---
+++ pinwords.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\n enter the number");
+scanf("%d",&n);
+switch(n)
+{
+case 1:
+printf("\n one");
+break;
+case 2:
+printf("\n two");
+break;
+case 3:
+printf("\n three");
+break;
+case 4:
+printf("\n four");
+break;
+default :
+printf("\n invalid ");
+}
+return 0;
+}
+
