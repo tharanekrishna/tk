39ae3b31cdb8775acc008bb6933888048d0c4c9b
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-21 16:02:50 +0530

    Create chknumalp.c

---
+++ chknumalp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[50]="hello78";
+
+if((a[50]>='a'||a[50]<='z')||(a[50]>='0'||a[50]<='9'))
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+return 0;
+}
