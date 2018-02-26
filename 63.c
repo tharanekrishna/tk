7029ade49699ad64765c8738607c7ca8f6c2e1c4
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-26 16:47:53 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[80];
+int i,n=0;
+scanf("%s",&a);
+for(i=0;i<a[i];i++)
+{
+if(a[i]==' ')
+{
+n++;
+}
+}
+printf("%d",n+1);
+return 0;
+}
