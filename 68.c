590f5de03738837f9261234487b28ed3277050b4
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-02-26 16:07:32 +0530

    Update 68.c

--- 68.c
+++ 68.c
@@ -2,12 +2,14 @@
 int main()
 {
     int a;
+    printf("\n enter the number :");
     scanf("%d",&a);
-  
     if(a%7==0)
-    printf("yes");
-    else
-    printf("no");
-    return 0;
     
+        printf("\n %d it is a multiple of 7",a);
+        else
+        printf("\n %d it is not a multiple of 7",a);
+        
+    
+    return 0;
 }
