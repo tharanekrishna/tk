bf395a2ed4efd481df7f529a592812d33a6de8ad
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-03-02 15:52:26 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[10],i;
+    scanf("%s",&a[i]);
+    for(i=0;i<5;i++)
+    if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+{
+    printf("\n yes");
+}
+else
+{
+    printf("\n no");
+    
+}
+return 0;
+
+}
