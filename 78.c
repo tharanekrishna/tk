8be6da8ca2844eb10e619c4751151cf11b0708ff
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-03-05 15:41:40 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("\n enter the number");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("\n no");
+        
+    
+    }
+
+    return 0;
+    
+}
