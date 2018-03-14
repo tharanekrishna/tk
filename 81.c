6e337ee1cc677e780688657bd56b5e7d2c44c9bf
tharanekrishna <30748363+tharanekrishna@users.noreply.github.com>
2018-03-14 16:08:04 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f,g,h,i;
+	scanf("%d%d",&a,&b);
+	g=a-b;
+	printf("\n %d",g);
+	scanf("%d%d",&c,&d);
+	h=c-d;
+	printf("\n%d",h);
+	scanf("%d%d",&e,&f);
+	i=e-f;
+	printf("\n%d",i);
+	return 0;
+	
+}
